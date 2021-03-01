973d08b804a029bb1bdf213056f45dd3f1c722d1
Syed Muhammad Mooazam <muhammadmoazam29@gmail.com>
2021-03-02 02:24:23 +0500

    added .ino file

---
+++ NodeMCU Blink Code.ino
@@ -0,0 +1,13 @@
+#define led D5
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
