837454726f8c6dd2862508cd6c39f51c3b900ff7
Syed Muhammad Mooazam <muhammadmoazam29@gmail.com>
2021-03-07 18:03:29 +0500

    Update NodeMCU_HTML_Webpage.ino

--- NodeMCU_HTML_Webpage.ino
+++ NodeMCU_HTML_Webpage.ino
@@ -10,7 +10,7 @@ void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  WiFi.begin("Dreamnet (( Mooazam ))", "Arham123");
+  WiFi.begin("Mooazam", "mooazam123");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(200);
