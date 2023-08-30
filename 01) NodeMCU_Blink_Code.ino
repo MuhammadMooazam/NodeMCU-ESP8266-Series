4a0f6d833dca2fd66d645424763d6504ff4463eb
Syed Muhammad Mooazam <muhammadmoazam29@gmail.com>
2023-08-30 21:22:28 +0500

    Update and rename NodeMCU Blink Code.ino to 01) NodeMCU_Blink_Code.ino

--- 01) NodeMCU_Blink_Code.ino
+++ 01) NodeMCU_Blink_Code.ino
@@ -1,11 +1,9 @@
 #define led D5
 void setup() {
-  // put your setup code here, to run once:
   pinMode(led, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(led, HIGH);
   delay(1000);
   digitalWrite(led, LOW);
